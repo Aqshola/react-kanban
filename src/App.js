57dd8e82230be89bbb1e977f668f64d267819bac
AqsholA <52363719+Aqshola@users.noreply.github.com>
2021-05-09 14:15:58 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="max-w-screen-xl h-screen">
+      <div className="w-full h-full flex justify-center items-center">
+        <h1 className="text-2xl">
+          This Project Bootstraped using{' '}
+          <b className="text-green-700 p-2 bg-blue-200 rounded-lg">
+            TailwindCSS
+          </b>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
